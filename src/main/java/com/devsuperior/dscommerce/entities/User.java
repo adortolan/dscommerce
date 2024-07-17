52b452e7f2b15ac39b7b27078505dda2f575c330
adortolan <adortolan@gmail.com>
2024-07-17 18:48:26 -0300

    Order, Enum, relacionamento muitos-para-um

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -17,6 +19,13 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private final List<Order> orders = new ArrayList<>();
+
+    public User(){
+
+    }
 
     public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
         this.id = id;
@@ -73,5 +82,9 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }

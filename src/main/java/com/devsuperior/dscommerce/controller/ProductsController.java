725c8fb2a18b5bb535a8f4ef056235ef61fd841a
adortolan <adortolan@gmail.com>
2024-07-19 22:19:46 -0300

    Busca paginada de produtos

--- src/main/java/com/devsuperior/dscommerce/controller/ProductsController.java
+++ src/main/java/com/devsuperior/dscommerce/controller/ProductsController.java
@@ -5,8 +5,11 @@ import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -19,5 +22,10 @@ public class ProductsController {
     @GetMapping(value ="/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         return productService.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return productService.findAll(pageable);
     }
 }

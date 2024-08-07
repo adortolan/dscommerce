849d5449535beeb960b00f61977cabc0499d0d25
adortolan <adortolan@gmail.com>
2024-08-07 07:50:59 -0300

    Adicionando controle de acesso por perfil e rota

--- src/main/java/com/devsuperior/dscommerce/controller/ProductsController.java
+++ src/main/java/com/devsuperior/dscommerce/controller/ProductsController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -39,6 +40,7 @@ public class ProductsController {
     }
 
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO productDTO) {
         productDTO = productService.insert(productDTO);
@@ -50,13 +52,13 @@ public class ProductsController {
         return ResponseEntity.created(uri).body(productDTO);
     }
 
-
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping(value ="/{id}")
     public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO productDTO) {
         return ResponseEntity.ok(productService.update(id, productDTO));
     }
 
-
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping(value ="/{id}")
     public ResponseEntity<Void> update(@PathVariable Long id) {
         productService.delete(id);

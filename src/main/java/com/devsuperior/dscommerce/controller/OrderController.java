50e7bcab95d259e634e425b7fea89424ba22a81a
adortolan <adortolan@gmail.com>
2024-08-10 11:29:09 -0300

    OrderDTO, busca de pedido por id

---
+++ src/main/java/com/devsuperior/dscommerce/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dscommerce.controller;
+
+import com.devsuperior.dscommerce.dto.OrderDTO;
+import com.devsuperior.dscommerce.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @GetMapping(value ="/{id}")
+    public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(orderService.findById(id));
+    }
+}
---
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
@@ -0,0 +1,71 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.entities.OrderItem;
+import com.devsuperior.dscommerce.entities.OrderStatus;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDTO {
+    private Long id;
+    private Instant momment;
+    private OrderStatus status;
+    private ClientDTO client;
+    private PaymentDTO payment;
+    private List<OrderItemDTO> items = new ArrayList<>();
+
+    public OrderDTO(Long id, Instant momment, OrderStatus status, ClientDTO client, PaymentDTO payment) {
+        this.id = id;
+        this.momment = momment;
+        this.status = status;
+        this.client = client;
+        this.payment = payment;
+    }
+
+    public OrderDTO(Order entity) {
+        id = entity.getId();
+        momment = entity.getMoment();
+        status = entity.getStatus();
+        client = new ClientDTO(entity.getClient());
+        payment = (entity.getPayment() == null) ? null : new PaymentDTO(entity.getPayment());
+
+        for (OrderItem item : entity.getItems()) {
+            OrderItemDTO dto = new OrderItemDTO(item);
+            items.add(dto);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Instant getMomment() {
+        return momment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public ClientDTO getClient() {
+        return client;
+    }
+
+    public PaymentDTO getPayment() {
+        return payment;
+    }
+
+    public List<OrderItemDTO> getItems() {
+        return items;
+    }
+
+    public Double getTotal() {
+        double sum = 0.0;
+        for (OrderItemDTO item : items) {
+            sum += item. getSubTotal();
+        }
+        return sum;
+    }
+}

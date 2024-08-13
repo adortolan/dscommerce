48189b35c6f049020f2f42936b90020478f289d2
adortolan <adortolan@gmail.com>
2024-08-13 14:16:30 -0300

    Salvando um pedido

--- src/main/java/com/devsuperior/dscommerce/controller/OrderController.java
+++ src/main/java/com/devsuperior/dscommerce/controller/OrderController.java
@@ -1,11 +1,16 @@
 package com.devsuperior.dscommerce.controller;
 
 import com.devsuperior.dscommerce.dto.OrderDTO;
+import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.services.OrderService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/orders")
@@ -19,5 +24,17 @@ public class OrderController {
     @GetMapping(value ="/{id}")
     public ResponseEntity<OrderDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(orderService.findById(id));
+    }
+
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@Valid @RequestBody OrderDTO orderDTO) {
+        orderDTO = orderService.insert(orderDTO);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(orderDTO.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(orderDTO);
     }
 }

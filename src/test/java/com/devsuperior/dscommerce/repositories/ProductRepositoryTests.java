bb8c35afd0f333d1a147b05bd1c61f994a32c316
adortolan <adortolan@gmail.com>
2024-08-16 16:06:11 -0300

    Fixtures no JUnit, BeforeEach

--- src/test/java/com/devsuperior/dscommerce/repositories/ProductRepositoryTests.java
+++ src/test/java/com/devsuperior/dscommerce/repositories/ProductRepositoryTests.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.repositories;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,9 +10,15 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 public class ProductRepositoryTests {
     @Autowired
     private ProductRepository repository;
+
+    private Long existingId;
+    @BeforeEach
+    private void setUp() throws Exception {
+        existingId = 1L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-        long existingId = 1L;
 
         repository.deleteById(existingId);
 

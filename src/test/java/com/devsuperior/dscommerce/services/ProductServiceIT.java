99aa11c6e682c9eed849b493b565663771b781dc
adortolan <adortolan@gmail.com>
2024-08-17 14:26:00 -0300

    Teste de integração para findAllPaged

--- src/test/java/com/devsuperior/dscommerce/services/ProductServiceIT.java
+++ src/test/java/com/devsuperior/dscommerce/services/ProductServiceIT.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dscommerce.services;
 
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.DataBaseException;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
@@ -9,6 +11,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @SpringBootTest
 public class ProductServiceIT {
@@ -51,5 +57,30 @@ public class ProductServiceIT {
         Assertions.assertThrows(DataBaseException.class, () -> {
             service.delete(dependentId);
         });
+    }
+
+    @Test
+    public void findAllShouldReturnAllProducts() {
+        Pageable pageable = PageRequest.of(0, 12);
+        Page<ProductMinDTO> result = service.findAll("", pageable);
+        Assertions.assertNotEquals(0, result.getTotalElements());
+    }
+
+    @Test
+    public void findAllShouldReturnPageSorted() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("name"));
+        Page<ProductMinDTO> result = service.findAll("", pageable);
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+        Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+    }
+
+    @Test
+    public void findAllShouldReturnPageSortedByPrice() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("price"));
+        Page<ProductMinDTO> result = service.findAll("", pageable);
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(90.5, result.getContent().get(0).getPrice());
+        Assertions.assertEquals(100.99, result.getContent().get(1).getPrice());
     }
 }
